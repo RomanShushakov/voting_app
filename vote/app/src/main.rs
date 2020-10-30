3393bf245945d4d340b81f56ab14040697ff07cb
roman shushakov <roman.a.shushakov@mail.ru>
2020-10-30 11:09:08 +0300

    actix connected to redis

--- vote/app/src/main.rs
+++ vote/app/src/main.rs
@@ -1,11 +1,17 @@
 use actix_web::
     {
         HttpServer, App, web, HttpResponse, HttpRequest, HttpMessage, error,
-        dev::HttpResponseBuilder, http::header, http::StatusCode,
+        dev::HttpResponseBuilder, http::header, http::StatusCode, middleware,
+        Error as AWError,
     };
 use actix_files::Files;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use derive_more::{Display, Error};
+use actix_redis::{Command, RedisActor};
+use actix::prelude::*;
+use futures::future::join_all;
+use redis_async::{resp::RespValue, resp_array};
+use serde_json;
 
 
 #[derive(Debug, Display, Error)]
@@ -35,7 +41,7 @@ impl error::ResponseError for MyError
 }
 
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 struct VoteRequest
 {
     voter_id: String,
@@ -43,7 +49,8 @@ struct VoteRequest
 }
 
 
-async fn vote(request: HttpRequest, vote_request: web::Json<VoteRequest>) -> Result<HttpResponse, MyError>
+async fn vote(request: HttpRequest, vote_request: web::Json<VoteRequest>, redis: web::Data<Addr<RedisActor>>)
+    -> Result<HttpResponse, MyError>
 {
     if let Ok(cookies) = request.cookies()
     {
@@ -51,7 +58,31 @@ async fn vote(request: HttpRequest, vote_request: web::Json<VoteRequest>) -> Res
         {
             if cookie.name() =="voter_id" && cookie.value() == vote_request.voter_id
             {
-                return Ok(HttpResponse::Ok().body("Vote registered."))
+                let vote = serde_json::to_string(&vote_request.into_inner()).unwrap();
+                let cmd = redis.send(Command(resp_array!["RPUSH", "votes", vote]));
+                let res: Vec<Result<RespValue, AWError>> =
+                    join_all(vec![cmd].into_iter())
+                        .await
+                        .into_iter()
+                        .map(|item|
+                            {
+                                item.map_err(AWError::from)
+                                    .and_then(|res| res.map_err(AWError::from))
+                            })
+                        .collect();
+
+                return if !res.iter().all(|res| match res
+                    {
+                        Ok(RespValue::Integer(_)) => true,
+                        _ => false,
+                    })
+                {
+                    Err(MyError::Unauthorized)
+                }
+                else
+                {
+                    Ok(HttpResponse::Ok().body("Your vote was registered."))
+                }
             }
         }
         Err(MyError::Unauthorized)
@@ -66,12 +97,21 @@ async fn vote(request: HttpRequest, vote_request: web::Json<VoteRequest>) -> Res
 #[actix_web::main]
 async fn main() -> std::io::Result<()>
 {
+    std::env::set_var("RUST_LOG", "actix_web=trace,actix_redis=trace");
+    env_logger::init();
+
     let bind = "0.0.0.0:8080";
     println!("Starting server at: {}", &bind);
 
+    dotenv::dotenv().ok();
+    let redis_addr = std::env::var("REDIS_ADDR").expect("REDIS_ADDR must be set");
+
     HttpServer::new(move ||
         {
+            let redis_addr = RedisActor::start(&redis_addr);
             App::new()
+                .data(redis_addr)
+                .wrap(middleware::Logger::default())
                 .route("/", web::post().to(vote))
                 .service(Files::new("", "./web_layout").index_file("index.html"))
         })
